ff85263566c6e4b71d4a0cc05bdced54ba107bfd
Heitor Santos <heitorg33@gmail.com>
2022-03-14 16:16:26 -0300

    Fix: refatorando codigo no service e controller

--- service/fetchApiService.js
+++ service/fetchApiService.js
@@ -2,8 +2,25 @@ const instance = require('../utils/request');
 
 const fetchApiService = async() => {
   const response = await instance.get('takenet/repos')
-  if(!response) return('Api não encontrada');
-  return response.data;
+  const date = response.data;
+  if(!date) return('Api não encontrada');
+  const searchData = await date.sort(function(a,b) {
+    const date1 = new Date(a.created_at);
+    const date2 = new Date(b.created_at);
+    return date1 - date2;
+  })
+  .map((elem) => {
+    if(elem.language === 'C#') {
+      return ({
+       name: elem.name,
+       description: elem.description,
+       avatar: elem.owner.avatar_url,
+      })
+    }
+  }).filter((elem) => {
+    if(elem !== null) return elem;
+  });
+  return searchData;
 }
 
 module.exports =  { fetchApiService } ;
